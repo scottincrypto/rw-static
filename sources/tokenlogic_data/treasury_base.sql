964815b8c1ac545b47d864ef63dd5f4f072f1e9c
Scott Simpson <scottincrypto@gmail.com>
2025-07-30 10:15:54 +0000

    fix: aggregate base query

--- sources/tokenlogic_data/treasury_base.sql
+++ sources/tokenlogic_data/treasury_base.sql
@@ -1,4 +1,18 @@
 select 
-  *
+  block_day
+  , protocol
+  , pool
+  , address
+  , holding_type
+  , symbol
+  , sum(balance) as balance
+  , sum(balance_usd) as balance_usd
 from datamart_treasury.stader_treasury_base
+group by 
+  block_day
+  , protocol
+  , pool
+  , address
+  , holding_type
+  , symbol
 order by block_day, pool
